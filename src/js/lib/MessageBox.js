afb6e2c0c25690a354c7f6587e38ca9cdc8244c2
Andrew Stewart Gibson <andrew.gibson@armakuni.com>
2022-08-31 14:21:00 +0100

    tweaking message box saving

--- src/js/lib/MessageBox.js
+++ src/js/lib/MessageBox.js
@@ -19,10 +19,10 @@ export default function MessageBox({ activationMessage, deactivationMessages, cl
             const concreteHTML = typeof html === "function" ? html(message) : html;
             dialog = document.createElement("DIALOG");
             dialog.className = className;
-            dialog.innerHTML = concreteHTML + `<div class="controls"><button class="ok">OK</button></div>`;
+            dialog.innerHTML = concreteHTML + `<div class="controls"><button class="close">Close</button></div>`;
             document.body.appendChild(dialog);
             dialog.addEventListener("close", () => dialog.remove());
-            dialog.querySelector("button.ok").addEventListener("click", () => dialog.remove());
+            dialog.querySelector("button.close").addEventListener("click", () => dialog.remove());
             dialog.showModal();
 
         } else if (deactivationMessages && deactivationMessages.includes(message.type)) {
